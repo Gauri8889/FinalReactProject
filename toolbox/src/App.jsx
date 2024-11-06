52d0ec2b884f87ab8e95c044bb9ed046e51d6f01
Gauri <dahakegauri4@gmail.com>
2024-11-06 22:27:10 +0530

    toolkit

--- toolbox/src/App.jsx
+++ toolbox/src/App.jsx
@@ -1,8 +1,20 @@
 import { useDispatch, useSelector } from "react-redux"
+import { colorChange } from "./bgcolorSlice";
 
 
 
 const App=()=>{
   const myclr=useSelector((state)=>state.mycolor.bgclr);
-  const dispatch=useDispatch()
+  const dispatch=useDispatch();
+  console.log(myclr);
+  return(
+    <>
+    <h1> This is my Counter Program</h1>
+    <button onClick={()=>{dispatch(colorChange())}}>click here</button>
+    <div style={{width:"300px", height:"200px", backgroundColor:myclr}}>
+
+    </div>
+    </>
+  )
 }
+export default App;
