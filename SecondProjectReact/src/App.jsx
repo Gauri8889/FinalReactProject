43ba8b5940f61fb53c33fd34a5528c227575ceea
Gauri <dahakegauri4@gmail.com>
2024-11-12 23:01:08 +0530

    SecondReactProject Changed

--- SecondProjectReact/src/App.jsx
+++ SecondProjectReact/src/App.jsx
@@ -1,11 +1,15 @@
 
 import TopMenu from "./Comonents/TopMenu";
+import TopBanner from "./Components/TopBanner";
+import StuCard from "./Components/StuCard";
 const App=()=>
 {
   return(
     <>
    
       <TopMenu/>
+      <TopBanner/>
+      <StuCard/>
 
     </>
   )
